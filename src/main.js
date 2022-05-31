bfb8d9b9e7b878e582678e34e8a0c0a55b9490c0
vinicenter <viniemidiobosi@gmail.com>
2022-05-31 14:35:26 -0300

    update dist branch for github pages

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import '@popperjs/core';
-import 'bootstrap';
-import 'phosphor-icons';
-
-import App from './App.vue';
-import router from './router';
-
-createApp(App).use(router).mount('#app');
